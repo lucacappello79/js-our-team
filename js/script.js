9526c3fd6df9f957bce6577e029fd1ed9e85e983
lucacappello79 <postt79@gmail.com>
2023-03-08 14:38:19 +0100

    feat: added logs

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let teamMembers = [
+    {
+      nome: "Wayne Barnett",
+      ruolo: "Founder & CEO	",
+      foto: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+      nome: "Angela Caroll",
+      ruolo: "Chief Editor",
+      foto: "angela-caroll-chief-editor.jpg"
+    },
+
+    {
+      nome: "Walter Gordon",
+      ruolo: "Office Manager",
+      foto: "walter-gordon-office-manager.jpg"
+    },
+
+    {
+      nome: "Angela Lopez",
+      ruolo: "Social Media Manager",
+      foto: "angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+      nome: "Scott Estrada",
+      ruolo: "Developer",
+      foto: "scott-estrada-developer.jpg"
+    },
+
+    {
+      nome: "Barbara Ramos",
+      ruolo: "Graphic Designer",
+      foto: "barbara-ramos-graphic-designer.jpg"
+    },
+
+  ];
+
+  for (let i = 0; i < teamMembers.length; i++) {
+
+    console.log("Nome: " + teamMembers[i].nome);
+    console.log("Ruolo: " + teamMembers[i].ruolo);
+    console.log("Foto: " + teamMembers[i].foto);
+
+  }
