91841a7fe7e377d4b63a8e5f9ebd08f8ec6ee7a3
lucacappello79 <postt79@gmail.com>
2023-03-08 17:50:08 +0100

    feat: changing code

--- js/script.js
+++ js/script.js
@@ -53,6 +53,8 @@ const UlFotoEl = document.getElementById("lista-foto");
 
 
 
+/* ------------2ND VERSION WITH PICTURES AND UL
+
 for (let i = 0; i < teamMembers.length; i++) {
 
   const memberName = teamMembers[i].nome;
@@ -82,11 +84,13 @@ for (let i = 0; i < teamMembers.length; i++) {
 
   UlFotoEl.append(picLiEl);
 }
+END OF 2ND VERSION ---- */
 
 
 
+/* 1ST VERSION WITH LIST ONLY
 
-/*const UlNomiEl = document.getElementById("lista-nomi");
+const UlNomiEl = document.getElementById("lista-nomi");
 const UlRoleEl = document.getElementById("lista-ruoli");
 const UlFotoEl = document.getElementById("lista-foto");
 
@@ -129,4 +133,44 @@ for (let i = 0; i < teamMembers.length; i++) {
 
   UlFotoEl.append(liEl);
 }
-*/
+----------------END OF 1ST VERSION */
+
+
+// BETA VERSION! TEST TEST TEST
+
+const cardContainerEl = document.getElementById("card-container");
+
+for (let i = 0; i < teamMembers.length; i++) {
+  const memberName = teamMembers[i].nome;
+  const memberRole = teamMembers[i].ruolo;
+  const memberFoto = teamMembers[i].foto;
+
+  // card 
+  const cardEl = document.createElement("div");
+  cardEl.classList.add("card", "col-3", "m-5");
+  cardEl.style.width = "18rem";
+
+  // image
+  const imgEl = document.createElement("img");
+  imgEl.src = "img/" + memberFoto;
+  imgEl.classList.add("card-img-top");
+
+  // body
+  const cardBodyEl = document.createElement("div");
+  cardBodyEl.classList.add("card-body");
+
+  // title
+  const cardTitleEl = document.createElement("h5");
+  cardTitleEl.classList.add("card-title", "text-center");
+  cardTitleEl.textContent = memberName;
+
+  // text
+  const cardTextEl = document.createElement("p");
+  cardTextEl.classList.add("card-text", "text-center");
+  cardTextEl.textContent = memberRole;
+
+  // append
+  cardBodyEl.append(cardTitleEl, cardTextEl);
+  cardEl.append(imgEl, cardBodyEl);
+  cardContainerEl.append(cardEl);
+}
